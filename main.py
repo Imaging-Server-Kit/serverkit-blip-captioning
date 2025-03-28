06634c9351d9d4f0d8bb06b8872b7038388fd9bf
Mallory Wittwer <mallory.wittwer@epfl.ch>
2025-03-28 16:18:41 +0100

    Update project

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Algorithm server definition.
 Documentation: https://github.com/Imaging-Server-Kit/cookiecutter-serverkit
 """
+
 from typing import List, Type
 from pathlib import Path
 import numpy as np
@@ -12,8 +13,10 @@ import imaging_server_kit as serverkit
 
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
+
 class Parameters(BaseModel):
     """Defines the algorithm parameters"""
+
     image: str = Field(
         ...,
         title="Image",
@@ -35,24 +38,26 @@ class Parameters(BaseModel):
             raise ValueError("Array has the wrong dimensionality.")
         return image_array
 
-class Server(serverkit.Server):
+
+class BlipCaptioningServer(serverkit.AlgorithmServer):
     def __init__(
         self,
-        algorithm_name: str="blip-captioning",
-        parameters_model: Type[BaseModel]=Parameters
+        algorithm_name: str = "blip-captioning",
+        parameters_model: Type[BaseModel] = Parameters,
     ):
         super().__init__(algorithm_name, parameters_model)
 
     def run_algorithm(
-        self,
-        image: np.ndarray,
-        conditional_text: str,
-        **kwargs
+        self, image: np.ndarray, conditional_text: str, **kwargs
     ) -> List[tuple]:
         """Runs the algorithm."""
-        processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-        model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
-        
+        processor = BlipProcessor.from_pretrained(
+            "Salesforce/blip-image-captioning-base"
+        )
+        model = BlipForConditionalGeneration.from_pretrained(
+            "Salesforce/blip-image-captioning-base"
+        )
+
         inputs = processor(image, conditional_text, return_tensors="pt")
         out = model.generate(**inputs)
         caption = processor.decode(out[0], skip_special_tokens=True)
@@ -65,8 +70,9 @@ class Server(serverkit.Server):
         images = [skimage.io.imread(image_path) for image_path in image_dir.glob("*")]
         return images
 
-server = Server()
+
+server = BlipCaptioningServer()
 app = server.app
 
-if __name__=='__main__':
+if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
